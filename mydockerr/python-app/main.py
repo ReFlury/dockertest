9e5a3bbf100ac78da6ac806feb7cb19082a823ac
Egor <egor.malinin2004@mail.ru>
2024-11-18 13:51:56 +0300

    Adding /mydockerr with tasks from 1st video(including docker-compose, js image, py image calendar and other)

---
+++ mydockerr/python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
+
+year = int(input('iNPUT YEAR: '))
+month = int(input('iNPUT month: '))
+
+print(calendar.month(year, month))
+
+print('Have a nice day!')
